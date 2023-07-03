58e3469d7446a0df04c08943f75e816044a75aea
hulliao <huliao@ucdavis.edu>
2023-07-03 16:45:06 -0700

    new

--- 22sumfac.py
+++ 22sumfac.py
@@ -7,7 +7,15 @@
 # Note: you may not import math or any other library
 
 n = 5 # use this value for your computation
+s = 0
+t = 1
+for i in range(1,n+1):
+    s +=i
+    t = t * i
+print(n,s,t)
 
+# factorial would be 1 * 2 * 3 * 4 * 5
+# use multiple variables
 
 """
 python3 22sumfac.py
