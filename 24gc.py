cd5bfb0b541a1b327ef267cba6d74197d57f75fc
hulliao <huliao@ucdavis.edu>
2023-07-04 15:29:27 -0700

    new

--- 24gc.py
+++ 24gc.py
@@ -3,8 +3,18 @@
 # Write a program that computes the GC% of a DNA sequence
 # Format the output for 2 decimal places
 
-dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
+# to calculate the percent:
+# its the number of G and C letters divided by the total letters in the dna sequence
 
+dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
+s = 0
+for i in range(len(dna)):
+    p = dna[i] 
+    if p == 'G' or p == 'C':
+        s += 1
+l = len(dna) 
+ans = s / l
+print(f'{ans:.2f}') 
 
 """
 python3 24gc.py
