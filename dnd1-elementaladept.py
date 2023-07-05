4f36e8e4921eaabca269e3f611e4d5d91e6da9c4
hulliao <huliao@ucdavis.edu>
2023-07-05 15:34:44 -0700

    new

--- dnd1-elementaladept.py
+++ dnd1-elementaladept.py
@@ -5,6 +5,24 @@
 # rolls of 1 become 2. How much more damage do you do on average if you are
 # an Elemental Adept? Simulate by rolling dice a million times.
 
+import random
+
+trials = 1000000
+fb = 0
+# start out as 0 and then the random function rolls the 10 sided dice
+for i in range(trials):
+    fb += random.randint(1,10) 
+
+ea = 0
+for j in range(trials):
+    roll = random.randint(1,10)
+    if roll == 1: roll = 2
+    ea += roll
+
+e = fb / trials
+f = ea / trials
+g = f - e
+print(f'{g:.1f}')
 
 """
 python3 dnd1-elementaladept.py
