ea8757bc3746071ffdc356a46a413fa56533bf8d
hulliao <huliao@ucdavis.edu>
2023-07-05 10:25:44 -0700

    new

--- 26anti.py
+++ 26anti.py
@@ -5,8 +5,24 @@
 
 # Variation: try this with the range() function and slice syntax
 
-dna = 'ACTGAAAAAAAAAAA'
+# How to reverse DNA sequence?
+# I want the sequence to print in the reverse direction
+# I also want the sequence to print the opposite base pair
 
+dna = 'ACTGAAAAAAAAAAA'
+seq = ''
+for i in range(len(dna)):
+    base = dna[i]
+    if base == 'A': seq += 'T'
+    elif base == 'T': seq += 'A'
+    elif base == 'C': seq += 'G'
+    else: seq += 'C'
+print(seq[::-1])
+
+
+a = ("abcdefgh") # testing different method
+x = slice(1, 5, 2)
+print(a[x])
 
 """
 python3 26anti.py
